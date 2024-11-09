9d5d7e2c7b349fe7f6b20306c1fd806bc3b842eb
Marmarossa <Mario.kodsi.94@gmail.com>
2024-11-09 20:48:15 +0100

    Created essential geometries and Player sprite.

--- main.py
+++ main.py
@@ -3,20 +3,22 @@
 # throughout this file
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()   
+    pyclock = pygame.time.Clock()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
 
-    while 1 < 5:
+    while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        pygame.display.flip()
         screen.fill("black")
+        dt = pyclock.tick(60)/1000
+        player.draw(screen)
+        pygame.display.flip()
 
 
 if __name__ == "__main__":
