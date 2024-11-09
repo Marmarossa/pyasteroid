6923de955e95921193c6c5574e0026e8fbcbbfe6
Marmarossa <Mario.kodsi.94@gmail.com>
2024-11-09 21:23:47 +0100

    Added Player movements and grouped similar objects together

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # this allows us to use code from
 # the open-source pygame library
 # throughout this file
-import pygame
+import pygame # type: ignore
 from constants import *
 from player import Player
 
@@ -10,15 +10,26 @@ def main():
     pyclock = pygame.time.Clock()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    updateable = pygame.sprite.Group(player)
+    drawable = pygame.sprite.Group(player)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
+        for object in updateable:
+            object.update(dt)
+
         screen.fill("black")
-        dt = pyclock.tick(60)/1000
-        player.draw(screen)
+
+        for object in drawable:
+            object.draw(screen)
+
         pygame.display.flip()
+
+        # limit framerate to 60 FPS
+        dt = pyclock.tick(60)/1000
 
 
 if __name__ == "__main__":
